7cb698956eb48ee9130a749bce87710a8d9c2788
EDnotSheeran <juninho.tab3@gmail.com>
2020-08-17 20:52:40 -0300

    funcao onibus

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,26 @@
 process.env.NTBA_FIX_319 = '1';
+import path  from 'path'
 import TelegramBot from 'node-telegram-bot-api'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const bot = new TelegramBot(process.env.TOKEN as string, {polling: true})
 
-// bot.on('message',(msg) => {
-//     bot.sendMessage(msg.chat.id,msg.text as string)
-// })
-
 bot.onText(/\/echo (.+)/, (msg, match) => {
     bot.sendMessage(msg.chat.id, match![1]);
 })
 
-bot.onText(/\/time/, (msg, match) => {
+bot.onText(/\/time/, (msg) => {
     const data = new Date()
-    bot.sendMessage(msg.chat.id, data.toTimeString());
+    bot.sendMessage(msg.chat.id, data.toTimeString())
+})
+
+bot.onText(/\/onibus/, (msg) => {
+    bot.sendPhoto(msg.chat.id, path.resolve(__dirname, 'assets', 'img', '5506.jpg'))
 })
 
 bot.on('new_chat_members',(msg) => {
-    bot.sendMessage(msg.chat.id,`Olá ${msg.new_chat_members![0].first_name}! Bem vindo(a)`);
+    bot.sendMessage(msg.chat.id,`Olá ${msg.new_chat_members![0].first_name}! Bem vindo(a)`)
 })
 
 console.log('\x1b[36mEDnotSheeran_bot\x1b[0m \x1b[33m[started]\x1b[0m')
